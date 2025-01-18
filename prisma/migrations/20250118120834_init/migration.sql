d74fa89db2a7400a3a973f610c82d707ccb656b9
JuannDarley <juandarley99@gmail.com>
2025-01-18 10:04:49 -0300

    Create CRUD equipaments

--- prisma/migrations/20250118120834_init/migration.sql
+++ prisma/migrations/20250118120834_init/migration.sql
@@ -1,8 +1,9 @@
 -- CreateTable
-CREATE TABLE "equipaments" (
+CREATE TABLE "Equipament" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "series" TEXT NOT NULL,
     "model" TEXT NOT NULL,
     "mark" TEXT NOT NULL,
-    "status" INTEGER
+    "status" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
--- src/routes/equipamentRoutes.ts
+++ src/routes/equipamentRoutes.ts
@@ -1,15 +1,103 @@
 import type { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { prisma } from '../lib/prisma'
 
 export async function equipamentRoutes(app: FastifyInstance) {
-	app.get('/equipament', async () => {
-		const equipaments = await prisma.equipament.findMany()
+	app.get('/equipaments', async () => {
+		const equipaments = await prisma.equipament.findMany({
+			orderBy: {
+				createdAt: 'asc',
+			},
+		})
 
-		return equipaments.map((equipamet) => {
+		return equipaments.map((equipaments) => {
 			return {
-				id: equipamet.id,
-				series: equipamet.series,
+				id: equipaments.id,
+				series: equipaments.series,
+				status: equipaments.status,
 			}
+		})
+	})
+
+	app.get('/equipaments/:id', async (request) => {
+		const paramsSchema = z.object({
+			id: z.string().uuid(),
+		})
+
+		const { id } = paramsSchema.parse(request.params)
+
+		const equipament = await prisma.equipament.findUniqueOrThrow({
+			where: {
+				id,
+			},
+		})
+
+		return equipament
+	})
+
+	app.post('/equipaments', async (request) => {
+		const bodySchema = z.object({
+			series: z.string(),
+			model: z.string(),
+			mark: z.string(),
+			status: z.number().int().nullable(),
+		})
+
+		const { series, model, mark, status } = bodySchema.parse(request.body)
+
+		const equipament = await prisma.equipament.create({
+			data: {
+				series,
+				model,
+				mark,
+				status,
+			},
+		})
+
+		return equipament
+	})
+
+	app.put('/equipaments/:id', async (request) => {
+		const paramsSchema = z.object({
+			id: z.string().uuid(),
+		})
+
+		const { id } = paramsSchema.parse(request.params)
+		const bodySchema = z.object({
+			series: z.string(),
+			model: z.string(),
+			mark: z.string(),
+			status: z.number().int().nullable(),
+		})
+
+		const { series, model, mark, status } = bodySchema.parse(request.body)
+
+		const equipament = await prisma.equipament.update({
+			where: {
+				id,
+			},
+			data: {
+				series,
+				model,
+				mark,
+				status,
+			},
+		})
+
+		return equipament
+	})
+
+	app.delete('/equipaments/:id', async (request) => {
+		const paramsSchema = z.object({
+			id: z.string().uuid(),
+		})
+
+		const { id } = paramsSchema.parse(request.params)
+
+		await prisma.equipament.delete({
+			where: {
+				id,
+			},
 		})
 	})
 }
